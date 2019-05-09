dd65650cd5297030dca4d7d49ad7c4514e3fd045
rafiffavian <rafiffavian1498@gmail.com>
2019-05-10 05:01:22 +0700

    jeroennotten

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin/rooms', 'Admin\RoomsController@index')->name('admin.rooms');
